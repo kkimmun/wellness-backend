a2de9a2082b69a023d31f438142ac2f2506a3aee
wonjunchae9797-beep <peony639@gmail.com>
2026-09-03 11:02:18 +0900

    feat:  지도 상세보기 기능 구현

--- src/main/java/com/kh/wellness/place/model/dao/PlaceMapper.java
+++ src/main/java/com/kh/wellness/place/model/dao/PlaceMapper.java
@@ -1,12 +1,21 @@
 package com.kh.wellness.place.model.dao;
 
-import org.apache.ibatis.annotations.Mapper;
-
 import com.kh.wellness.course.model.dto.PlaceDto;
+import com.kh.wellness.place.model.dto.PlaceDetailDto;
+import com.kh.wellness.place.model.dto.PlaceImageDto;
+import com.kh.wellness.place.model.dto.PlaceTagDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import java.util.List;
 
 @Mapper
 public interface PlaceMapper {
 
-	PlaceDto selectByPlaceNo(Long placeNo);
+    // 팀원(main) 코드
+    PlaceDto selectByPlaceNo(Long placeNo);
 
+    // 고객님 코드 (상세보기)
+    PlaceDetailDto selectPlaceDetail(@Param("placeNo") Long placeNo, @Param("memberNo") Long memberNo);
+    List<PlaceImageDto> selectPlaceImages(@Param("placeNo") Long placeNo);
+    List<PlaceTagDto> selectPlaceTags(@Param("placeNo") Long placeNo);
 }
