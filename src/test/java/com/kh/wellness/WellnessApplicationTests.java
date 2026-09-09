3f6c6d7b0940fa862b15b30a7a49d9c4b44292a5
koyong3941 <koyong3941@gmail.com>
2026-09-09 10:22:37 +0900

    fix : test 수정 및 장소 타입에 따른 거리 찾기 수정

--- src/test/java/com/kh/wellness/WellnessApplicationTests.java
+++ src/test/java/com/kh/wellness/WellnessApplicationTests.java
@@ -21,8 +21,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 		"mybatis.configuration.jdbc-type-for-null=VARCHAR",
 		"mybatis.configuration.map-underscore-to-camel-case=true",
 		"mybatis.configuration.log-impl=org.apache.ibatis.logging.stdout.StdOutImpl",
-		"mybatis.mapper-locations=classpath:mapper/**/*.xml",
-		"mybatis.type-aliases-package=com.kh.wellness.member.model.vo,com.kh.wellness.token.model.vo,com.kh.wellness.member.model.dto,com.kh.wellness.admin.place.model.dto,com.kh.wellness.admin.course.model.dto,com.kh.wellness.course.model.dto,com.kh.wellness.course.model.enums,com.kh.wellness.route.model.vo,com.kh.wellness.place.model.vo"
+		"mybatis.mapper-locations=classpath:mapper/**/*.xml"
 })
 class WellnessApplicationTests {
 
