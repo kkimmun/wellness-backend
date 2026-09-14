60e5b2345080b76b63e8a672ae828ba4dafc0200
koyong3941 <koyong3941@gmail.com>
2026-09-14 17:59:14 +0900

    센서 및 log 제거

--- src/main/java/com/kh/wellness/sensor/model/dto/SensorRequestDto.java
+++ src/main/java/com/kh/wellness/sensor/model/dto/SensorRequestDto.java
@@ -4,6 +4,9 @@ import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,8 +19,14 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SensorRequestDto {
+	@NotNull
+	@Positive
 	private Long sensorNo;
+	@NotNull
+	@Positive
 	private Long infoNo;
+	@NotNull
+	@PositiveOrZero
 	private Long stepCount;
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 	private LocalDateTime collectTime;
